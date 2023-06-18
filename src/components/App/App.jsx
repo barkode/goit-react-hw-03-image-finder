2a45e27d4f10e347aa97d4228e27225be9481039
barkode <konstantinks@gmail.com>
2023-06-18 01:18:15 +0100

    add modal
    add modal window. Add portal. Add functions to modal class

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,5 +1,5 @@
 import css from './App.module.css';
-import React from 'react';
+import { Component } from 'react';
 import { Searchbar } from 'components/Searchbar/Searchbar';
 import { SearchForm } from 'components/SearchForm/SearchForm';
 import { ImageGallery } from 'components/ImageGallery/ImageGallery';
@@ -7,17 +7,31 @@ import { ImageGalleryItem } from 'components/ImageGalleryItem/ImageGalleryItem';
 import { Button } from 'components/Buton/Button';
 import { Modal } from 'components/Modal/Modal';
 
-export const App = () => {
-  return (
-    <div className={css.App}>
-      <Searchbar>
-        <SearchForm />
-      </Searchbar>
-      <ImageGallery>
-        <ImageGalleryItem />
-      </ImageGallery>
-      <Button>Load more</Button>
-      <Modal />
-    </div>
-  );
-};
+export class App extends Component {
+  state = {
+    showModal: false,
+  };
+
+  toogleModal = () => {
+    this.setState(({ showModal }) => ({ showModal: !showModal }));
+  };
+
+  render() {
+    const { showModal } = this.state;
+    return (
+      <div className={css.App}>
+        <Searchbar>
+          <SearchForm />
+        </Searchbar>
+        <ImageGallery>
+          <ImageGalleryItem />
+        </ImageGallery>
+        <Button>Load more</Button>
+        <button type="button" onClick={this.toogleModal}>
+          Modal
+        </button>
+        {showModal && <Modal onClose={this.toogleModal} />}
+      </div>
+    );
+  }
+}
--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,11 +1,41 @@
+import { Component } from 'react';
+import { createPortal } from 'react-dom';
 import css from './Modal.module.css';
 
-export const Modal = () => {
-  return (
-    <div className={css.overlay}>
-      <div className={css.modal}>
-        <img src="" alt="" />
-      </div>
-    </div>
-  );
-};
+const modalRoot = document.querySelector('#root-modal');
+
+export class Modal extends Component {
+  componentDidMount() {
+    window.addEventListener('keydown', this.handleKeyDown);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('keydown', this.handleKeyDown);
+  }
+
+  handleKeyDown = e => {
+    if (e.code === 'Escape') {
+      this.props.onClose();
+    }
+  };
+
+  handleBackdropClick = e => {
+    if (e.currentTarget === e.target) {
+      this.props.onClose();
+    }
+  };
+
+  render() {
+    return createPortal(
+      <div className={css.Overlay} onClick={this.handleBackdropClick}>
+        <div className={css.Modal}>
+          <img
+            src="https://images.freeimages.com/vhq/images/previews/f1e/skull-drawing-vector-16480.jpg"
+            alt=""
+          />
+        </div>
+      </div>,
+      modalRoot
+    );
+  }
+}
