dd0ada211368ffe90f632dd08c6fa4fdf33a4679
barkode <konstantinks@gmail.com>
2023-06-19 12:38:09 +0100

    make render gallery

--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -1,23 +1,40 @@
 import { Component } from 'react';
+import { getImages } from 'utils/fetch_api';
 import css from './ImageGalleryItem.module.css';
 
 export class ImageGalleryItem extends Component {
+  state = {
+    images: null,
+  };
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevProps.searchText !== this.props.searchText) {
+      getImages(this.props.searchText)
+        .then(res => {
+          if (!res.ok) {
+            throw new Error(res.status);
+          }
+          return res.json();
+        })
+        .then(images => this.setState({ images: images.hits }));
+    }
+  }
   render() {
+    const { images } = this.state;
+    console.log(this.state.images);
     return (
-      <>
-        <li className={css.ImageGalleryItem}>
-          <img src="" alt="" className={css['ImageGalleryItem-image']} />
-        </li>
-        <li className={css.ImageGalleryItem}>
-          <img src="" alt="" className={css['ImageGalleryItem-image']} />
-        </li>
-        <li className={css.ImageGalleryItem}>
-          <img src="" alt="" className={css['ImageGalleryItem-image']} />
-        </li>
-        <li className={css.ImageGalleryItem}>
-          <img src="" alt="" className={css['ImageGalleryItem-image']} />
-        </li>
-      </>
+      images &&
+      images.map(({ id, tags, webformatURL }) => {
+        return (
+          <li key={id} className={css.ImageGalleryItem}>
+            <img
+              src={webformatURL}
+              alt={tags}
+              className={css['ImageGalleryItem-image']}
+            />
+          </li>
+        );
+      })
     );
   }
 }
