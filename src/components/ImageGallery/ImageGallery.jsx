e854debb4407893e109343a72ed075c35513d1b1
barkode <konstantinks@gmail.com>
2023-06-19 12:37:55 +0100

    add paramets to state

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -2,6 +2,10 @@ import { Component } from 'react';
 import css from './ImageGallery.module.css';
 
 export class ImageGallery extends Component {
+  state = {
+    showModal: false,
+    loading: false,
+  };
   render() {
     const { children } = this.props;
     return <ul className={css.ImageGallery}>{children}</ul>;
